2a07e803548e67b571d4f921209004e766bb3c38
Chris Dell <cjdell@gmail.com>
2025-12-01 11:47:24 +0000

    Add rollback and list-generations script

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    # ==== Packages ====
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        list-generations = (pkgs.writers.writeNuBin "list-generations" ./nu/list-generations.nu);
+      in
+      {
+        packages.default = list-generations;
+      }
+    )
+    # ==== Modules ====
+    // {
+      modules.rollback = ./rollback.nix;
+    };
+}
